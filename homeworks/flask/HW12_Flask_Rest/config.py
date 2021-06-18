3126385f9cb6d086e9d7ce06fac79110a7f6e6d6
Mikhail <mikhail.chabanenko@gmail.com>
2021-06-19 01:36:36 +0300

    Done

---
+++ homeworks/flask/HW12_Flask_Rest/config.py
@@ -0,0 +1,7 @@
+import os
+
+
+class Config:
+    WEATHER_API_KEY = "7c124f7569mshe0a45eeb13c04fep1365a4jsna4e8bf6b0090"
+    WEATHER_API_URL = "https://community-open-weather-map.p.rapidapi.com/find"
+    WEATHER_API_HOST = "community-open-weather-map.p.rapidapi.com"
