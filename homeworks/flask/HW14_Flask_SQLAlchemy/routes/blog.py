42d1ad7505f009b1dfdc6e32bac1786adab2cf78
Mikhail <Mikhail.chabanenko@gmail>
2021-07-08 20:58:28 +0300

    Done

--- homeworks/flask/HW14_Flask_SQLAlchemy/routes/blog.py
+++ homeworks/flask/HW14_Flask_SQLAlchemy/routes/blog.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from app import app, api, db
 from flask import render_template, request, Response
 from config import Config, articles
@@ -5,23 +7,23 @@ from flask_restful import Resource, Api
 from models.models import Article, User
 
 
-@app.route('/', methods=["GET"])
+@app.route("/", methods=["GET"])
 def homepage():
     articles = Article.query.all()
-    return render_template('blog/index.html', config=Config, articles=articles)
+    return render_template("blog/index.html", config=Config, articles=articles)
 
 
-@app.route('/article/<string:slug>')
+@app.route("/article/<string:slug>")
 def article_details(slug):
     article = Article.query.filter_by(slug=slug).first()
-    return render_template('blog/details.html', article=article)
+    return render_template("blog/details.html", article=article, username=find_user_by_id(article.author_id))
 
 
 class MenuItem(Resource):
     def get(self):
         return {
-            'success': True,
-            'items': Config.MENU_ITEMS
+            "success": True,
+            "items": Config.MENU_ITEMS
         }
 
 
@@ -29,12 +31,12 @@ class Articles(Resource):
     def post(self):
         data = request.json
         article = Article(
-            title=data.get('title'),
-            slug=data.get('slug'),
-            author_id=data.get('author_id'),
-            description=data.get('description'),
-            short_description=data.get('short_description'),
-            img=data.get('img')
+            title=data.get("title"),
+            slug=data.get("slug"),
+            author_id=data.get("author_id"),
+            description=data.get("description"),
+            short_description=data.get("short_description"),
+            img=data.get("img")
         )
         db.session.add(article)
         db.session.commit()
@@ -42,10 +44,10 @@ class Articles(Resource):
 
     def get(self):
         articles = Article.query
-        if request.args.get('title'):
-            articles = articles.filter_by(title=request.args.get('title'))
+        if request.args.get("title"):
+            articles = articles.filter_by(title=request.args.get("title"))
 
-        # articles = articles.filter(Article.title.startswith('A'))
+        # articles = articles.filter(Article.title.startswith("A"))
 
         if request.args.get("sort_by"):
             articles = articles.order_by(request.args.get("sort_by"))
@@ -61,71 +63,77 @@ class Articles(Resource):
 class ArticlesEntity(Resource):
     def get(self, id):
         article = Article.query.get(id)
-        if article == None:
+        if article is None:
             return Response(status=404)
         return article.serialize
 
 
 class Users(Resource):
     def get(self):
-        user = User.query.get(1)
+        users = User.query
+        if request.args.get("id"):
+            users = users.filter_by(id=request.args.get("id"))
+        if request.args.get("name"):
+            users = users.filter_by(username=request.args.get("name"))
+        if request.args.get("is_admin"):
+            users = users.filter_by(admin=request.args.get("is_admin"))
+        if request.args.get("sort_by"):
+            users = users.order_by(request.args.get("sort_by"))
+        all_users = users.all()
+        serialized_users = []
+        for user in all_users:
+            serialized_users.append(user.serialize)
+
+        return serialized_users
+
+
+class UserRead(Resource):
+    def get(self, id):
+        user = User.query.get(id)
+        if user is None:
+            return Response(status=404)
         serialized_articles = []
         for article in user.articles:
             print(article)
             serialized_articles.append(article.serialize)
-        return serialized_articles
+        serialized_user = [user.serialize, serialized_articles]
+        return serialized_user
 
 
-class User_Update(Resource):
-    def post(self,id):
-        user = User.query.get(id)
-        username = request.json['username']
-        email = request.json['email']
-
-        user.email = email
-        user.username = username
+class UserUpdate(Resource):
+    def put(self, id):
+        data = request.json
+        user = User.query.filter_by(id=id)
+        user.update(data)
         db.session.commit()
+        return User.query.get(id).serialize
 
-        return user.serialize
 
-
-class User_Create(Resource):
+class UserCreate(Resource):
     def post(self):
         data = request.json
         user = User(
-            username=data.get('username'),
-            email=data.get('email'),
-            created=data.get('created'),
-            bio=data.get('bio'),
-            admin=data.get('admin')
+            username=data.get("username"),
+            email=data.get("email"),
+            created=str(datetime.now()),
+            bio=data.get("bio"),
+            admin=data.get("admin")
         )
-        if user is None:
-            db.session.add(user)
-            db.session.commit()
-            return user.serialize, 201
-        else:
-            abort(409, f'User {user.username} exists already')
+        db.session.add(user)
+        db.session.commit()
+        return user.serialize
 
 
-class User_Read(Resource):
-    def get(self, id):
-        user = User.query.get(id)
-        if user is not None:
-            serialized_articles = []
-            for article in user.articles:
-                serialized_articles.append(article.serialize)
-            serialized_user = [user.serialize, serialized_articles]
-            return serialized_user
-        else:
-            abort(404, 'Person not found for Id: {id}'.format(id=id))
+def find_user_by_id(id):
+    user = User.query.get(id)
+    username = user.username
+    return username
 
 
-
-api.add_resource(MenuItem, '/menu-items')
-api.add_resource(Articles, '/articles')
-api.add_resource(Users, '/users')
-api.add_resource(ArticlesEntity, '/articles/<int:id>')
-api.add_resource(User_Read, '/users/<int:id>')
-api.add_resource(User_Update, '/users/update/<int:id>')
-api.add_resource(User_Create, '/users/create')
-
+api.add_resource(MenuItem, "/menu-items")
+api.add_resource(Articles, "/articles")
+api.add_resource(ArticlesEntity, "/articles/<int:id>")
+api.add_resource(Users, "/users")
+api.add_resource(UserRead, "/users/<int:id>")
+api.add_resource(UserUpdate, "/users/update/<int:id>")
+api.add_resource(UserCreate, "/users/create")
